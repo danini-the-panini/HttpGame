40d6ee055f50c6f598745f55e0795e1b87472a30
Daniel Smith <jellymann@gmail.com>
2013-05-20 19:45:11 +0200

    Fixed board.js

--- board.js
+++ board.js
@@ -6,12 +6,16 @@ function board(row,col)
     {
         if (xmlhttp.readyState==4 && xmlhttp.status==200)
         {
+            alert("D");
             document.getElementById("temp").innerHTML=xmlhttp.responseText;
         }
     }
     
-    xmlhttp.open("POST"."Play.x",true);
+    xmlhttp.open("POST","Play.x",true);
+    alert("A");
     xmlhttp.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+    alert("B");
     xmlhttp.send("row="+row+"&col="+col);
+    alert("C");
     
 }
